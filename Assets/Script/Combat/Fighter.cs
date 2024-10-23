64b1de3aec8dcf92636998f3a2b487e84c9d0e1a
chinawin47 <chinawin47@gmail.com>
2024-10-23 22:20:31 +0700

    Attck hit target

---
+++ Assets/Script/Combat/Fighter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attack(CombatTarget target)
+        {
+            print("Take that you short, squat peasant!");
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
