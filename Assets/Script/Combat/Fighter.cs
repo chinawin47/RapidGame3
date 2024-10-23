4bb2a0a716af7b41d50d96e8967e0459cf3f86c7
chinawin47 <chinawin47@gmail.com>
2024-10-23 23:44:12 +0700

    Cancel for Attack

--- Assets/Script/Combat/Fighter.cs
+++ Assets/Script/Combat/Fighter.cs
@@ -1,27 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Movement;
 
 namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour
     {
-        public void Attack(CombatTarget target)
-        {
-            print("Take that you short, squat peasant!");
-        }
-
-
-        // Start is called before the first frame update
-        void Start()
-        {
-
-        }
+        [SerializeField] float weaponRange = 2f;
+        
+        Transform target;
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
-
+            if (target == null) return;
+            if (target != null && !GetIsInRange())
+            {
+                GetComponent<Mover>().MoveTo(target.position);
+            }
+            else
+            {
+                GetComponent<Mover>().Stop();
+            }
         }
+
+        private bool GetIsInRange()
+        {
+            return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+
+        public void Attack(CombatTarget combatTarget)
+        {
+            target = combatTarget.transform;
+        }
+        
+        public void Cancel()
+        { 
+            target = null; 
+        }
+
     }
 }
