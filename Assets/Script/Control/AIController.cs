4a5f9a7ef799c019d2b6cd2cc77b90ba5ba818dd
chinawin47 <chinawin47@gmail.com>
2024-10-27 00:23:22 +0700

    Ai Guarding Position

--- Assets/Script/Control/AIController.cs
+++ Assets/Script/Control/AIController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using RPG.Core;
+using RPG.Movement;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 
@@ -13,13 +14,19 @@ namespace RPG.Control
 
         Fighter fighter;
         Health health;
+        Mover mover;
         GameObject player;
+
+        Vector3 guardPosition;
 
         private void Start()
         {
             fighter = GetComponent<Fighter>();
             health = GetComponent<Health>();
+            mover = GetComponent<Mover>();
             player = GameObject.FindWithTag("Player");
+
+            guardPosition = transform.position;
         }
 
         private void Update()
@@ -32,7 +39,7 @@ namespace RPG.Control
             }
             else
             {
-                fighter.Cancel();
+                mover.StartMoveAction(guardPosition);
             }
         }
 
@@ -41,5 +48,13 @@ namespace RPG.Control
             float distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
             return distanceToPlayer < chaseDistance;
         }
+
+        //Called by Unity
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+
     }
 }
