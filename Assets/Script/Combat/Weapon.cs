4c5f77353531fe7f0db68da11b29e59b0af81a72
chinawin47 <chinawin47@gmail.com>
2024-10-27 22:11:03 +0700

    Bug sword now

--- Assets/Script/Combat/Weapon.cs
+++ Assets/Script/Combat/Weapon.cs
@@ -7,15 +7,21 @@ namespace RPG.Combat
     [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Make New Weapon", order = 0)]
     public class Weapon : ScriptableObject
     {
-        [SerializeField] GameObject weaponPrefad = null;
+        [SerializeField] GameObject equippedPrefad = null;
         [SerializeField] AnimatorOverrideController animatorOverride = null;
         [SerializeField] float weaponDamage = 5f;
         [SerializeField] float weaponRange = 2f;
 
         public void Spawn(Transform handTransfor, Animator animator)
         {
-            Instantiate(weaponPrefad, handTransfor);
-            animator.runtimeAnimatorController = animatorOverride;
+            if(equippedPrefad != null) 
+            {
+               Instantiate(equippedPrefad, handTransfor);
+            }
+            if(animatorOverride != null)
+            {
+                animator.runtimeAnimatorController = animatorOverride;
+            }
         }
 
         public float GetDamage() 
